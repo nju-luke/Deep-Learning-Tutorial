6678b3032bda569abdb8e192f508186d2a6b0c4d
nju-luke <nju.hyhb@gmail.com>
2016-04-12 10:39:13 +0800

    Added files via upload

--- LoadData.m
+++ LoadData.m
@@ -1,27 +1,31 @@
 % Load MNIST DataSet
-% [images,labels,test_images,test_labels,imageDim] = LoadData(бо2D' ); %Load for 2D
-% [images,labels,test_images,test_labels,imageDim] = LoadData( '1D' ); %Load for 1D
+% [images,labels,test_images,test_labels,imageDim] = LoadData(para); 
+% para.type = '1D' or '2D'
+% para.num is the numbers of samples
 
-function [images,labels,test_images,test_labels,imageDim] = LoadData(type,num)
+function [images,labels,test_images,test_labels,imageDim] = LoadData(para)
 %% Load Data
 
 images = loadMNISTImages('train-images-idx3-ubyte');
 labels = loadMNISTLabels('train-labels-idx1-ubyte');
-labels(labels==0) = 10; % Remap 0 to 10
-
-images = images(:,1:num);
-labels = labels(1:num);
+labels(labels == 0) = 10;
 
 test_images = loadMNISTImages('t10k-images-idx3-ubyte');
 test_labels = loadMNISTLabels('t10k-labels-idx1-ubyte');
-test_labels(labels==0) = 10; % Remap 0 to 10
+test_labels(test_labels == 0) = 10;
 imageDim = size(images,1);
 
-test_images = test_images(:,1:num);
-test_labels = test_labels(1:num);
+if isfield(para,'num')
+    num = para.num;
+    images = images(:,1:num);
+    labels = labels(1:num);
+    test_images = test_images(:,1:num);
+    test_labels = test_labels(1:num);
+end
 
-if type == '2D'
+if strcmp(para.type, '2D')
     imageDim = sqrt(imageDim);
     images = reshape(images,imageDim,imageDim,[]);
     test_images = reshape(test_images,imageDim,imageDim,[]);
 end
+
